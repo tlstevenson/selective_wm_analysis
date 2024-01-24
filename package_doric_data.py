848cebe66b61fd0e1c5e7e72fc0b7bfc5a348fc0
tstevenson <tstevenson@ucdavis.edu>
2024-01-24 01:14:08 -0800

    Fixed minor bugs in packaging code

--- package_doric_data.py
+++ package_doric_data.py
@@ -9,32 +9,37 @@ Created on Wed Nov 15 23:30:18 2023
 
 import init
 import os.path as path
+from pyutils import utils
 from sys_neuro_tools import doric_utils as dor
 from sys_neuro_tools import acq_utils as acq
 import pickle
 import matplotlib.pyplot as plt
 
-def package_data(data, save_path, ttl_name = 'ttl', target_dt = 0.005):
+def package_data(data, save_paths, ttl_name = 'ttl', target_dt = 0.005):
     ''' Decimate and save data to a pkl file '''
 
     # get trial start timestamps
     trial_start_ts, trial_nums = acq.parse_trial_times(data[ttl_name]['values'], data[ttl_name]['time'])
 
-    dec_time, dec_signals, dec_info = acq.decimate_data(data, target_dt = target_dt)
+    signal_data = {k:v for k,v in data.items() if k != ttl_name}
+    dec_time, dec_signals, dec_info = acq.decimate_data(signal_data, target_dt = target_dt)
 
     save_data = {'trial_start_ts': trial_start_ts, 'trial_nums': trial_nums,
                  'time': dec_time, 'signals': dec_signals, 'decimation': dec_info}
 
-    with open(save_path, 'wb') as f:
-        pickle.dump(save_data, f)
+    if not utils.is_list(save_paths):
+        save_paths = [save_paths]
+        
+    for save_path in save_paths:
+        with open(save_path, 'wb') as f:
+            pickle.dump(save_data, f)
 
     return save_data
 
 # %% Declare file & look at structure
-
-filename = 'Session_94072'
+sess_id = 93580
+filename = 'Session_{}'.format(sess_id)
 load_path = path.join('E:', 'Data', 'Rat 179', filename+'.doric')
-save_path = path.join('E:', 'Data', 'Rat 179', filename+'.pkl')
 
 dor.h5print(load_path)
 
@@ -68,18 +73,19 @@ if len(issues) > 0:
     print('Issues found:\n{0}'.format('\n'.join(issues)))
 
 # %% Package data
-dec_data = package_data(data, save_path)
+save_paths = [path.join('E:', 'Data', 'Rat 179', filename+'.pkl'), path.join(utils.get_user_home(), 'fp_data', filename+'pkl')]
+dec_data = package_data(data, save_paths)
 
 # %% Plot comparison of raw and decimated data
 
-fig, axs = plt.subplots(2,2, layout='constrained', figsize=(12,10))
-axs = axs.flatten()
+# fig, axs = plt.subplots(2,2, layout='constrained', figsize=(12,10))
+# axs = axs.flatten()
 
-for i, signal_name in enumerate([k for k in data.keys() if k != 'ttl']):
-    ax = axs[i]
-    ax.plot(data[signal_name]['time'], data[signal_name]['values'], label='Raw Data')
-    ax.plot(dec_data['time'], dec_data['signals'][signal_name], label='Decimated Data')
-    ax.set_title(signal_name)
+# for i, signal_name in enumerate([k for k in data.keys() if k != 'ttl']):
+#     ax = axs[i]
+#     ax.plot(data[signal_name]['time'], data[signal_name]['values'], label='Raw Data')
+#     ax.plot(dec_data['time'], dec_data['signals'][signal_name], label='Decimated Data')
+#     ax.set_title(signal_name)
 
-lines, labels = axs[0].get_legend_handles_labels()
-fig.legend(lines, labels)
+# lines, labels = axs[0].get_legend_handles_labels()
+# fig.legend(lines, labels)
