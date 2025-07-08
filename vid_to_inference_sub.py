436842fc498803a75db2d7fe5598f42cd593ed57
AlexHackathon <alexandru.tapus@gmail.com>
2025-07-08 15:52:44 -0700

    Smoothed and Unsmoothed Outlier Detection
    With both smoothed and unsmoothed positions, inference_to_outliers_main takes an hdf5 file and can show what various thresholds for acceleration return as outliers.

--- vid_to_inference_sub.py
+++ vid_to_inference_sub.py
@@ -10,11 +10,20 @@ import sys
 import init
 import json
 from sys_neuro_tools import sleap_utils_env
+import subprocess
+#import os
 sleap_settings_path = sys.argv[1]
 with open(sleap_settings_path, "r") as file:
     sleap_settings = json.load(file)
     vid_path = sleap_settings["vid_path"]
+    #UNTESTED
+    #new_name = vid_path[:-4] + "_reformat.mp4"
+    #Reformats the given video
+    #if not os.path.isfile(new_name):
+        #command = f'ffmpeg -y -i {vid_path} -c:v libx264 -pix_fmt yuv420p -preset superfast -crf 23 {new_name}'
+        #subprocess.run(command)
     centroid_path = sleap_settings["centroid_path"]
     center_path = sleap_settings["center_path"]
     write_dir = sleap_settings["write_dir"]
     sleap_utils_env.RunInference(vid_path, centroid_path, center_path, write_dir)
+    #sleap_utils_env.RunInference(new_name, centroid_path, center_path, write_dir)
