91d9f8f35950232758d9db8566b37e4e4dbbd9ca
hankslab <hankslab@ucdavis.edu>
2026-04-06 16:00:40 -0700

    Latest changes to packaging script

--- package_doric_data.py
+++ package_doric_data.py
@@ -17,16 +17,12 @@ import os
 import os.path as path
 import numpy as np
 
-rec_date = date(2026,4,3)
+rec_date = date(2026,4,6)
 
-# subj_ids = [198]
-# subj_region_dict = {s: {'PL': 2, 'DLS': 1, 'DMS': 3 } for s in subj_ids}#,'TS': 4} #'PL': 1, 'DMS': 2  }#
-# wavelength_dict = {490: 2, 420: 1, 465: 4, 405: 3} #420: 1, 465: 4, 490: 2, , 405: 3
-
-subj_region_dict = {483: {'NAc-L': 1, 'NAc-R': 4, 'TS-L': 2, 'TS-R': 3},
-                    238: {'NAc': 3, 'DMS': 4, 'DLS': 2, 'TS': 1},
-                    237: {'PL': 2, 'NAc': 4, 'DMS': 3, 'TS': 1},
-                    424: {'PL': 2, 'NAc': 4, 'DMS': 3, 'DLS': 1}}
+subj_region_dict = {483: {'NAc-L': 2, 'NAc-R': 1, 'TS-L': 4, 'TS-R': 3},
+                    238: {'NAc': 1, 'DMS': 3, 'DLS': 4, 'TS': 2},
+                    237: {'PL': 1, 'NAc': 3, 'DMS': 4, 'TS': 2},
+                    424: {'PL': 1, 'NAc': 3, 'DMS': 4, 'DLS': 2}}
 
 wavelength_channel_dict = {1: 420, 2: 490, 3: 415, 4: 490}
 io_channel_map = {1: [1,2], 2: [1,2], 3: [3,4], 4: [3,4]} # input channels to output channels
@@ -78,7 +74,7 @@ for subj_id in subj_ids:
                                print_file_struct = print_struct, print_attr = print_attr, io_channel_map=io_channel_map)
         
         # rename file with session id
-        new_name = path.join(root_dir, 'session_{}'.format(sess_id))
+        new_name = path.join(root_dir, 'Session_{}.doric'.format(sess_id))
         os.rename(data_file, new_name)
 
 # %% Add Manually
@@ -91,21 +87,25 @@ for subj_id in subj_ids:
 
 # %% Rename old files
 
-subj_ids = [198,199,274,400,402,237,238,424,483]
-data_dir = 'D:/Tanner'
+# subj_ids = [198,199,274,400,402,237,238,424,483]
+# data_dir = 'D:/Tanner'
 
-for subj_id in subj_ids:
-    root_dir = path.join(data_dir, str(subj_id))
-    subj_data_files = [path.join(root_dir, f) for f in glob.glob('*.doric', root_dir=root_dir)]
-    file_times = [datetime.fromtimestamp(pathlib.Path(f).stat().st_ctime, tz = timezone.utc) for f in subj_data_files]
+# for subj_id in subj_ids:
+#     root_dir = path.join(data_dir, str(subj_id))
+#     subj_data_files = [path.join(root_dir, f) for f in glob.glob('*.doric', root_dir=root_dir)]
+#     file_times = [datetime.fromtimestamp(pathlib.Path(f).stat().st_ctime, tz = timezone.utc) for f in subj_data_files]
     
-    for data_file, file_time in zip(subj_data_files, file_times):
-        if sum([f_time.date() == file_time.date() for f_time in file_times]) > 1:
-            print('Found multiple sessions on the same day for subject {} on date {}. Please add them individually. Continuing...'.format(subj_id, file_time.date().isoformat()))
-            continue
+#     for data_file, file_time in zip(subj_data_files, file_times):
+#         if sum([f_time.date() == file_time.date() for f_time in file_times]) > 1:
+#             print('Found multiple sessions on the same day for subject {} on date {}. Please rename them individually. Continuing...'.format(subj_id, file_time.date().isoformat()))
+#             continue
         
-        subj_sess_ids = db_access.get_subj_sess_ids_by_date(subj_id, file_time.date().isoformat())
+#         subj_sess_ids = db_access.get_subj_sess_ids_by_date([subj_id], file_time.date().isoformat())
         
-        # rename file with session id
-        new_name = path.join(root_dir, 'session_{}'.format(subj_sess_ids[subj_id][0]))
-        os.rename(data_file, new_name)
+#         if len(subj_sess_ids[subj_id]) > 1:
+#             print('Found multiple sessions on the same day for subject {} on date {}. Please rename them individually. Continuing...'.format(subj_id, file_time.date().isoformat()))
+#             continue
+        
+#         # rename file with session id
+#         new_name = path.join(root_dir, 'Session_{}.doric'.format(subj_sess_ids[subj_id][0]))
+#         os.rename(data_file, new_name)
