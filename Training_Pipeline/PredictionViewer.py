7191982a711a13008c3e932bd20aa681bf523318
Alexandru Tapus <aitapus@ucdavis.edu>
2026-06-16 09:16:11 -0700

    Fully Functional Threshold Visualization
    Threshold visualization works by toggling with 0. Fixed an issue where closed windows were still keeping active threads and required kernel restart by waiting an extra bit at the end to ensure proper closure.

--- Training_Pipeline/PredictionViewer.py
+++ Training_Pipeline/PredictionViewer.py
@@ -16,8 +16,33 @@ DATA_PATH = r"C:\Users\cns-th-lab\SLEAP_Labels_198_199x_237x_238x_274x_400x_402x
 OUTPUT_WINDOW = "Keypoint Inspector"
 FPS = 30 # Defined conversion rate
 
-# Load tracking data
-#df = pd.read_hdf(DATA_PATH) 
+# --- Transformation and Filtering Functions ---
+
+def ThresholdedPositions(positions, scores, threshold):
+    mask = scores < threshold
+    positions[mask] = np.nan
+    return positions
+
+
+
+with h5py.File(DATA_PATH, "r") as f:
+    # Decode node names
+    node_names = [n.decode('utf-8') for n in f['node_names'][:]]
+    
+    # 1. Get Prediction Scores 
+    # Raw shape: (tracks, nodes, frames) -> Transposed: (frames, nodes, tracks)
+    scores = np.transpose(f['point_scores'][:], (2, 1, 0)) 
+    
+    # 2. Get Coordinates
+    # Raw shape: (tracks, nodes, 2, frames) -> Transposed: (frames, nodes, 2, tracks)
+    tracks_coords = np.transpose(f['tracks'][:])
+    
+    # 3. Get Thresholded Coordinates (Only 1 score for both so must index separately)
+    thresh_coords_x = ThresholdedPositions(tracks_coords[:,:,0,:], scores, .3)
+    thresh_coords_y = ThresholdedPositions(tracks_coords[:,:,1,:], scores, .3)
+    
+        
+# --- Setup and main loop
 
 cap = cv2.VideoCapture(VIDEO_PATH)
 total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -32,20 +57,16 @@ print("[I]                : Change Interval")
 print("[F]                : Jump to specific Frame")
 print("[S]                : Jump to specific Second")
 print("[Q]                : Quit")
+print("[0-9]              : Toggle Transformation Slots")
 print("----------------")
 
-with h5py.File(DATA_PATH, "r") as f:
-    # Decode node names
-    node_names = [n.decode('utf-8') for n in f['node_names'][:]]
-    
-    # 1. Get Prediction Scores 
-    # Raw shape: (tracks, nodes, frames) -> Transposed: (frames, nodes, tracks)
-    scores = np.transpose(f['point_scores'][:], (2, 1, 0)) 
-    
-    # 2. Get Coordinates
-    # Raw shape: (tracks, nodes, 2, frames) -> Transposed: (frames, nodes, 2, tracks)
-    tracks_coords = np.transpose(f['tracks'][:])
-        
+transformation_0 = False
+transformation_1 = False
+transformation_2 = False
+transformation_3 = False
+transformation_4 = False
+transformation_5 = False
+
 while True:
     # Seek to the current target frame
     cap.set(cv2.CAP_PROP_POS_FRAMES, frame_idx)
@@ -65,11 +86,8 @@ while True:
             if np.isnan(current_pos[i,0]) or np.isnan(current_pos[i,1]):
                 continue
             
-            print(i)
             x = int(current_pos[i,0])
-            print(x)
             y = int(current_pos[i,1])
-            print(y)
             
             # Draw the keypoint if coordinates are valid
             if x > 0 and y > 0:
@@ -80,11 +98,36 @@ while True:
         print("No tracking data this frame")
         pass # No tracking data for this frame
 
+    #Transformation Handling
+    if transformation_0:
+        # Extract coordinates for the current frame
+        try:
+            print(np.shape(thresh_coords_x))
+            curr_thresh_coords_x = thresh_coords_x[frame_idx,:,0]
+            curr_thresh_coords_y = thresh_coords_y[frame_idx,:,0]
+            print(np.shape(curr_thresh_coords_x))
+            
+            # Iterating through keypoints
+            for i in range(np.shape(curr_thresh_coords_x)[0]):
+                if np.isnan(curr_thresh_coords_x[i]) or np.isnan(curr_thresh_coords_y[i]):
+                    continue
+                
+                x = int(curr_thresh_coords_x[i])
+                y = int(curr_thresh_coords_y[i])
+                
+                # Draw the keypoint if coordinates are valid
+                if x > 0 and y > 0:
+                    cv2.circle(frame, center=(x+2, y), radius=5, color=(255, 0, 0), thickness=-1)
+                    #cv2.putText(frame, f"{node_names[i]}: {round(current_scores[i][0],2)}", (x + 8, y - 8), 
+                    #            cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1)
+        except IndexError:
+            print("No tracking data this frame")
+            pass # No tracking data for this frame
+
     # Overlay frame and time index text
     current_time = frame_idx / FPS
     cv2.putText(frame, f"Frame: {frame_idx}/{total_frames - 1} | Time: {current_time:.2f}s", 
                 (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
-
     cv2.imshow(OUTPUT_WINDOW, frame)
     
     # Keyboard Navigation
@@ -131,6 +174,23 @@ while True:
             
     elif key == ord('q'): # Quit
         break
+    
+    #Transformation toggles
+    elif key == ord('0'):
+        transformation_0 = not transformation_0
+        print(f"Threshold coords toggled: {transformation_0}")
+    elif key == ord('1'):
+        transformation_1 = not transformation_1
+    elif key == ord('2'):
+        transformation_2 = not transformation_2
+    elif key == ord('3'):
+        transformation_3 = not transformation_3
+    elif key == ord('4'):
+        transformation_4 = not transformation_4
+    elif key == ord('0'):
+        transformation_5 = not transformation_5
 
+    
 cap.release()
 cv2.destroyAllWindows()
+cv2.waitKey(1)
